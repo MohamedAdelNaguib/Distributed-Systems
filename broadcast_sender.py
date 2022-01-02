27384a08dd2bc4611bc9a28ab7840027d4ca228b
R. Erdem Uysal <erdemuysal@yahoo.com>
2022-01-02 17:51:55 +0100

    broadcast_listener.py and broadcast_sender.py have been updated

--- broadcast_sender.py
+++ broadcast_sender.py
@@ -1,9 +1,12 @@
 import socket
+import pickle
 
 
-def broadcast(ip, port, broadcast_message):
+def send_broadcast(ip, port, broadcast_message):
     # Create a UDP socket
     broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # Send message on broadcast address
     broadcast_socket.sendto(str.encode(broadcast_message), (ip, port))
     broadcast_socket.close()
@@ -17,8 +20,21 @@ if __name__ == '__main__':
     # Local host information
     MY_HOST = socket.gethostname()
     MY_IP = socket.gethostbyname(MY_HOST)
+    print('My IP:', MY_IP)
+
+    BUFFER_SIZE = 4096
+    GROUP_VIEW = []
 
     # Send broadcast message
     message = MY_IP + ' sent a broadcast'
-    broadcast(BROADCAST_IP, BROADCAST_PORT, message)
+    send_broadcast(BROADCAST_IP, BROADCAST_PORT, message)
+    print("Sent a broadcast")
 
+    print("Listening for a reply")
+    # Create a UDP socket
+    listen_reply = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    while True:
+        data, addr = listen_reply.recvfrom(BUFFER_SIZE)
+        if data:
+            print("Received broadcast message:", data.decode())
